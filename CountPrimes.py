4f9814d65498887ae70876beb6ce8009c8c70820
danyshman <danyshman.azamatov@gmail.com>
2019-06-15 11:47:36 +0600

    CountPrimes was solved

---
+++ CountPrimes.py
@@ -0,0 +1,16 @@
+def SieveOfEratosthenes(n):
+    prime = [True for i in range(n + 1)]
+    p = 2
+    while (p * p <= n):
+        if (prime[p] == True):
+            for i in range(p * 2, n + 1, p):
+                prime[i] = False
+        p += 1
+    c = 0
+    for p in range(2, n):
+        if prime[p]:
+            c += 1
+    return c
+
+
+print(SieveOfEratosthenes(1500000))
