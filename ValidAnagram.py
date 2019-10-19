d28657c0fa2d9676310a15140f0c9eb4f4a0ac2d
danyshman <danyshman.azamatov@gmail.com>
2019-10-19 12:57:07 +0600

    ValidAnagram was solved

---
+++ ValidAnagram.py
@@ -0,0 +1,16 @@
+def isAnagram(s, t):
+    s_dict = dict()
+    t_dict = dict()
+    for letter in s:
+        s_dict[letter] = s_dict.get(letter, 0) + 1
+    for letter in t:
+        t_dict[letter] = t_dict.get(letter, 0) + 1
+    for key, value in t_dict.items():
+        if value != s_dict.get(key, 0):
+            return False
+    for key, value in s_dict.items():
+        if value != t_dict.get(key, 0):
+            return False
+    return True
+
+print(isAnagram("ab","a"))
