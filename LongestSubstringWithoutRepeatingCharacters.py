7902673b33c96efcad3892bf73c13c116c066f72
danyshman <danyshman.azamatov@gmail.com>
2019-11-24 18:37:35 +0600

    LongestSubstringWithoutRepeatingCharacters.py was solved

--- LongestSubstringWithoutRepeatingCharacters.py
+++ LongestSubstringWithoutRepeatingCharacters.py
@@ -1,26 +1,15 @@
-def lengthOfLongestSubstring(s):
-    if len(s) == 0:
-        return 0
-    if len(s) == 1:
-        return 1
-    if s[0] * len(s) == s:
-        return 1
-    longest_substring = ''
-    for i in range(len(s)):
-        char_indexes = dict()
-        char_indexes[s[i]] = True
-        temp = s[i]
-        for j in range(i+1, len(s)):
-            if char_indexes.get(s[j], False) is False and j == len(s)-1 and len(temp)+1 > len(longest_substring):
-                longest_substring = temp + s[j]
-            if char_indexes.get(s[j], False):
-                if len(temp) > len(longest_substring):
-                    longest_substring = temp
-                break
-            else:
-                char_indexes[s[j]] = True
-                temp += s[j]
-    return len(longest_substring)
+def lengthOfLongestSubstring(self, s: str) -> int:
+    global_max = temp_max = start = 0
+    d = dict()
+    for index, letter in enumerate(s):
+        if letter in d and d[letter] >= start:
+            global_max = max(global_max, temp_max)
+            temp_max = index - d[letter]
+            start = d[letter] + 1
+        else:
+            temp_max += 1
+        d[letter] = index
+    return max(global_max, temp_max)
 
 
-print(lengthOfLongestSubstring("tmmzuxt"))
+print(lengthOfLongestSubstring("pwwkew"))
