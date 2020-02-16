82887be66922705b40141761796838fa55f7eb17
danyshman <danyshman.azamatov@gmail.com>
2020-02-16 15:50:39 +0300

    ScoreOfParentheses.py was updated

--- ScoreOfParentheses.py
+++ ScoreOfParentheses.py
@@ -1,29 +1,28 @@
-def scoreOfParantheses(S):
-    temp = list(S)
-    i = 0
-    while i < len(temp) - 1:
-        if temp[i] == '(' and temp[i + 1] == ')':
-            temp = temp[:i] + ['1'] + temp[i + 2::]
-        else:
-            i += 1
-    if len(temp) == 1:
-        return int(temp[0])
-    while '(' in temp or len(temp) > 1:
-        i = 1
-        while len(temp) - 1 > i:
-            if temp[i] not in '()' and temp[i - 1] == '(' and temp[i + 1] == ')':
-                temp = temp[:i - 1] + [str(2 * int(temp[i]))] + temp[i + 2::]
-                i -= 1
+class Solution:
+    def scoreOfParentheses(self, S: str) -> int:
+        arr = []
+        for p in S:
+            if p == '(':
+                arr.append(p)
             else:
-                i += 1
-        i = 0
-        while i < len(temp) - 1:
-            if temp[i] not in '()' and temp[i + 1] not in '()':
-                temp = temp[:i] + [str(int(temp[i]) + int(temp[i + 1]))] + temp[i + 2::]
-            else:
-                i += 1
-        S = ''.join(temp)
-    return int(S)
-
-
-print(scoreOfParantheses("(()(()))"))
+                if arr[-1] == '(':
+                    arr.pop()
+                    if arr and arr[-1] not in ['(', ')']:
+                        arr[-1] += 1
+                    else:
+                        arr.append(1)
+                elif arr[-2] == '(':
+                    num = arr.pop()*2
+                    arr.pop()
+                    if arr and arr[-1] not in ['(', ')']:
+                        arr[-1] += num
+                    else:
+                        arr.append(num)
+                else:
+                    num = arr.pop()+arr.pop()
+                    arr.pop()
+                    if arr and arr[-1] not in ['(', ')']:
+                        arr[-1] += num
+                    else:
+                        arr.append(num)
+        return sum(arr)
