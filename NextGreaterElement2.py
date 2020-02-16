fe5ae1d20d6f4d83305bb78dc702bd0a38f77f8d
danyshman <danyshman.azamatov@gmail.com>
2020-02-16 19:52:15 +0300

    NextGreaterElement2.py was updated

--- NextGreaterElement2.py
+++ NextGreaterElement2.py
@@ -1,16 +1,13 @@
-def nextGreaterElements(nums):
-    ln = len(nums)
-    stack = []
-    ans = [-1 for __ in range(ln)]
-    for times in range(2):
-        for i in range(ln-1, -1, -1):
-            while stack and stack[-1] <= nums[i]:
-                stack.pop()
-            if stack:
-                ans[i] = stack[-1]
-            stack.append(nums[i])
-    return ans
-
-
-print(nextGreaterElements([-3,2,-2,1,-1,0,-3,-1]))
-
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        ans = [-1] * len(nums)
+        st = []
+        for i in range(len(nums)):
+            while st and nums[i] > nums[st[-1]]:
+                ans[st.pop()] = nums[i]
+            st.append(i)
+        for i in range(len(nums[::-1])):
+            while st and nums[i] > nums[st[-1]]:
+                ans[st.pop()] = nums[i]
+            st.append(i)
+        return ans
