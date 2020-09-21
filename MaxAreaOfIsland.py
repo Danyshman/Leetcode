9bb5afef79c5a7068c3acf260a7fa3cbb647884f
danyshman <danyshman.azamatov@latoken.com>
2020-09-21 15:22:53 +0300

    Max Area Of Island

--- MaxAreaOfIsland.py
+++ MaxAreaOfIsland.py
@@ -1,39 +1,18 @@
-def maxAreaOfIsland(grid):
-    max_area = 0
-    global_checked_points = []
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == 1 and (i,j) not in global_checked_points:
-                checked_points = []
-                should_check = [(i,j)]
-                area = 1
-                while len(should_check) != 0:
-                    for point in should_check:
-                        global_checked_points.append(point)
-                        if point[1] != 0:
-                            if grid[point[0]][point[1] - 1] == 1 and (point[0], point[1] - 1) not in checked_points and (point[0], point[1] - 1) not in should_check:
-                                should_check.append((point[0], point[1] - 1))
-                                area += 1
-                        if point[0] != 0:
-                            if grid[point[0] - 1][point[1]] == 1 and (point[0] - 1, point[1]) not in checked_points and (point[0] - 1, point[1]) not in should_check:
-                                should_check.append((point[0] - 1, point[1]))
-                                area += 1
-                        if point[1] != len(grid[point[0]]) - 1:
-                            if grid[point[0]][point[1] + 1] == 1 and (point[0], point[1] + 1) not in checked_points and (point[0], point[1] + 1) not in should_check:
-                                should_check.append((point[0], point[1] + 1))
-                                area += 1
-                        if point[0] != len(grid) - 1:
-                            if grid[point[0] + 1][point[1]] == 1 and (point[0] + 1, point[1]) not in checked_points and (point[0] + 1, point[1]) not in should_check:
-                                should_check.append((point[0] + 1, point[1]))
-                                area += 1
-                        checked_points.append((point[0], point[1]))
-                        should_check.remove((point[0], point[1]))
-                if area > max_area:
-                    max_area = area
-
-    return max_area
-
-
-print(maxAreaOfIsland([[1,1,1],
-                       [0,1,1]]))
-
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        def dfs(r,c):
+            grid[r][c] = 0
+            count = 1
+            for rr,cc in [[r+1, c], [r-1,c], [r, c+1], [r, c-1]]:
+                if 0 <= rr < h and 0 <= cc < w and grid[rr][cc]:
+                    count += dfs(rr,cc)
+            return count
+        if not grid:
+            return 0
+        h, w = len(grid), len(grid[0])
+        max_area = 0
+        for i in range(h):
+            for j in range(w):
+                if grid[i][j]:
+                    max_area = max(max_area, dfs(i, j))
+        return max_area
